4f659d5d3b3e329f2add74cadfeea9fc00784a65
yootaeyun <ew20@naver.com>
2022-07-22 02:11:22 +0900

    To-Do 앱 : class컴포넌트에서 함수형 컴포넌트로 바꾸기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 // import SpreadOperator from "./SpreadOperator";
 
-// Class컴포넌트로 만들기
-export default class App extends Component {
-  // State 생성
-  state = {
-    todoData: [
-      {
-        id: "1",
-        title: "ㅎㅇ",
-        completed: false,
-      },
-    ],
-    value: "",
-  };
+export default function App() {
+  const [todoData, setTodoData] = useState([]);
+  const [value, setValue] = useState("");
 
-  btnStyle = {
+  const btnStyle = {
     color: "#000",
     border: "none",
     padding: "5px 9px",
@@ -26,7 +16,7 @@ export default class App extends Component {
   };
 
   // completed값이 true(체크)면 선긋기
-  getStyle = (completed) => {
+  const getStyle = (completed) => {
     return {
       padding: "15px 5px 15px 5px",
       borderBottom: "1px dotted #000",
@@ -34,20 +24,20 @@ export default class App extends Component {
     };
   };
 
-  handleClick = ({ dataId }) => {
-    let newTodoData = this.state.todoData.filter((data) => data.id !== dataId);
+  const handleClick = ({ dataId }) => {
+    let newTodoData = todoData.filter((data) => data.id !== dataId);
     console.log("newTodoData", newTodoData);
-    this.setState({ todoData: newTodoData });
+    setTodoData(newTodoData);
   };
 
-  handleChange = (e) => {
-    this.setState({ value: e.target.value });
+  const handleChange = (e) => {
+    setValue(e.target.value);
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (this.state.value === "") {
+    if (value === "") {
       alert("내용을 입력해주세요!");
       return;
     }
@@ -55,71 +45,71 @@ export default class App extends Component {
     // 새로운 할 일 데이터
     let newTodo = {
       id: Date.now(), // id는 유니크한 값
-      title: this.state.value,
+      title: value,
       completed: false,
     };
 
     // 원래 있던 할일 목록에 새로운 할 일 더해주고, 입력은 빈값 처리
-    this.setState({ todoData: [...this.state.todoData, newTodo], value: "" });
+    // prev 함수는 이전 State값을 가져옴
+    setTodoData((prev) => [...prev, newTodo]);
+    setValue("");
   };
 
   // 체크 박스 선택시 줄긋기
   // 해당 id값을 받고, 받은 id랑,  todoData의 id랑 일치하면, todoData의 completed값을 변경시킴
-  handleCompleChange = ({ dataId }) => {
-    let newTodoData = this.state.todoData.map((data) => {
+  const handleCompleChange = ({ dataId }) => {
+    let newTodoData = todoData.map((data) => {
       if (data.id === dataId) {
         data.completed = !data.completed;
       }
       return data;
     });
-    this.setState({ todoData: newTodoData });
+    setTodoData(newTodoData);
   };
 
-  render() {
-    return (
-      <div className="conteiner">
-        {/* <SpreadOperator /> */}
-        <div className="todoBlock">
-          <div className="title">
-            <h1>할 일 목록</h1>
-          </div>
-
-          {/* JSX Key 속성이란 : 요소의 리스트를 나열할때는 Key를 넣어줘야함, React가 변경, 추가 또는 제거된 항목을 식별하는데 도움을줌. 요소에 안정적인 ID를 부여하려면 배열 내부 요소에 Key를 제공해야함 */}
-          {this.state.todoData.map((data) => (
-            <div style={this.getStyle(data.completed)} key={data.id}>
-              <input
-                type="checkbox"
-                defaultChecked={false}
-                onChange={() => this.handleCompleChange({ dataId: data.id })}
-              />
-              {data.title}
-              <button
-                style={this.btnStyle}
-                onClick={() => this.handleClick({ dataId: data.id })}
-              >
-                x
-              </button>
-            </div>
-          ))}
-
-          <form style={{ display: "flex" }} onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              name="value"
-              style={{ flex: "10", padding: "5px" }}
-              placeholder="할 일을 입력하세요."
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-            <input
-              type="submit"
-              value="입력"
-              className="btn"
-              style={{ flex: "1" }}
-            />
-          </form>
+  return (
+    <div className="conteiner">
+      {/* <SpreadOperator /> */}
+      <div className="todoBlock">
+        <div className="title">
+          <h1>할 일 목록</h1>
         </div>
+
+        {/* JSX Key 속성이란 : 요소의 리스트를 나열할때는 Key를 넣어줘야함, React가 변경, 추가 또는 제거된 항목을 식별하는데 도움을줌. 요소에 안정적인 ID를 부여하려면 배열 내부 요소에 Key를 제공해야함 */}
+        {todoData.map((data) => (
+          <div style={getStyle(data.completed)} key={data.id}>
+            <input
+              type="checkbox"
+              defaultChecked={false}
+              onChange={() => handleCompleChange({ dataId: data.id })}
+            />
+            {data.title}
+            <button
+              style={btnStyle}
+              onClick={() => handleClick({ dataId: data.id })}
+            >
+              x
+            </button>
+          </div>
+        ))}
+
+        <form style={{ display: "flex" }} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="value"
+            style={{ flex: "10", padding: "5px" }}
+            placeholder="할 일을 입력하세요."
+            value={value}
+            onChange={handleChange}
+          />
+          <input
+            type="submit"
+            value="입력"
+            className="btn"
+            style={{ flex: "1" }}
+          />
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
